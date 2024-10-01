a24e25a1a6dbf52f43d2c97bfe49b60a215db6e1
Adam Buxton <buxton.adam@gmail.com>
2024-10-01 10:22:32 +0100

    add overrides and workarounds

---
+++ override_locals.tf
@@ -0,0 +1,3 @@
+locals {
+  template_path = var.template_file != "" ? "${path.module}/templates/tfe_user_data.sh.tpl" : "${path.cwd}/templates/${var.template_file}"
+}
---
+++ override_variables.tf
@@ -0,0 +1,19 @@
+variable "template_file" {
+  type    = string
+  default = "tfe_user_data.sh.tpl"
+  validation {
+    condition     = can(fileexists("../../templates/${var.template_file}") || fileexists("./templates/${var.template_file}"))
+    error_message = "File `./templates/${var.template_file}` not found or not readable"
+  }
+}
+
+variable "container_runtime" {
+  type        = string
+  description = "Container runtime to use for TFE. Supported values are 'docker' or 'podman'."
+  default     = "docker"
+
+  validation {
+    condition     = contains(["podman", "docker"], var.container_runtime)
+    error_message = "Supported values are `docker` or `podman`."
+  }
+}
