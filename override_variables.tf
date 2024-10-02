01e11c48b9bb5faab28b1afb856d679573cf7eae
Adam Buxton <buxton.adam@gmail.com>
2024-10-02 13:25:19 +0100

    update conditonal

--- override_variables.tf
+++ override_variables.tf
@@ -1,6 +1,6 @@
 variable "template_file" {
   type    = string
-  default = "tfe_user_data.sh.tpl"
+  default = ""
   validation {
     condition     = can(fileexists("../../templates/${var.template_file}") || fileexists("./templates/${var.template_file}"))
     error_message = "File `./templates/${var.template_file}` not found or not readable"
